12ef470ddd25b603b0fb8e63f22bf69ac2a44d83
dmitry.weirdo <dmitry.weirdo@8e0f0ab9-060e-e749-fcf0-e6a1db176365>
2010-11-23 21:43:24 +0000

    Добавлен модуль util.

---
+++ trunk/util/src/ru/pda/chemistry/util/Transliterate.java
@@ -0,0 +1,13 @@
+package ru.pda.chemistry.util;
+
+/**
+ * User: 1
+ * Date: 20.04.2010
+ * Time: 21:02:13
+ */
+public class Transliterate
+{
+	public static String transliterate(String russian) {
+		return "";
+	}
+}
