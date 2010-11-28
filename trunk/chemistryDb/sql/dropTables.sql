0a564c0583445bb3fd56825a6c38be64a9c15d6c
dmitry.weirdo <dmitry.weirdo@8e0f0ab9-060e-e749-fcf0-e6a1db176365>
2010-11-28 19:39:17 +0000

    Создание и удаление таблиц переделано на Firebird. Удаление таблиц вынесено в dropTables.sql.

---
+++ trunk/chemistryDb/sql/dropTables.sql
@@ -0,0 +1,13 @@
+drop table Enum;
+drop generator enum_gen;
+
+drop table Attribute;
+drop generator attribute_gen;
+
+drop table Entity;
+drop generator entity_gen;
+
+drop table Section;
+drop generator section_gen;
+
+commit;
