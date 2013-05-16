fe14f94016f0989c2df093ff41274e719d552fee
dmitry.weirdo <dmitry.weirdo@gmail.com>
2013-05-16 20:07:04 +0000

    Скрипт таблицы стал создавать ячейки для ссылок на создание, изменение и удаление.

--- datadirWeb/web/js/pureJsTable.js
+++ datadirWeb/web/js/pureJsTable.js
@@ -2,6 +2,8 @@
  * No usage of any js-library framework.
  * // todo: namespace
  */
+var NBSP_CODE = '\u00a0';
+
 function createEntityTable(config) {
 	// todo: validate config
 
@@ -27,6 +29,13 @@ function createEntityTable(config) {
 		var attributeHeaderCell = dh.createEl(dh.TH_TAG, null, null, attribute.label);
 		headerRow.appendChild(attributeHeaderCell);
 	}
+
+	var updateHeader = dh.createEl(dh.TH_TAG, null, null, 'Изменить');
+	var deleteHeader = dh.createEl(dh.TH_TAG, null, null, 'Удалить');
+
+	headerRow.appendChild(updateHeader);
+	headerRow.appendChild(deleteHeader);
+
 	tableBody.appendChild(headerRow);
 
 
@@ -40,15 +49,46 @@ function createEntityTable(config) {
 			attribute = config.metadata.attributes[j];
 			value = record[attribute.name];
 			if (!value)
-				value = '\u00a0'; // todo: move to NBSP_CODE constant and renderer
+				value = NBSP_CODE; // todo: move to NBSP_CODE constant and renderer
 
 			// todo: apply value renderer
 			var valueCell = dh.createEl(dh.TD_TAG, null, null, value);
 			recordRow.appendChild(valueCell);
 		}
 
+		var updateCell = dh.createEl(dh.TD_TAG);
+		var updateLink = dh.createEl(dh.A_TAG, null, null, 'Изменить');
+		updateLink.setAttribute(dh.HREF_ATTRIBUTE, '/update?entityId=' + metadata.id + '&recordId=' + record.id);
+		updateCell.appendChild(updateLink);
+
+		var deleteCell = dh.createEl(dh.TD_TAG);
+		var deleteLink = dh.createEl(dh.A_TAG, null, null, 'Удалить');
+		deleteLink.setAttribute(dh.HREF_ATTRIBUTE, '/delete?entityId=' + metadata.id + '&recordId=' + record.id);
+		deleteCell.appendChild(deleteLink);
+
+		recordRow.appendChild(updateCell);
+		recordRow.appendChild(deleteCell);
+
 		tableBody.appendChild(recordRow);
 	}
+
+	var createRow = dh.createEl(dh.TR_TAG);
+	var leftCreateCell = dh.createEl(dh.TD_TAG, null, null, NBSP_CODE);
+	leftCreateCell.setAttribute(dh.COL_SPAN_ATTRIBUTE, config.metadata.attributes.length);
+	leftCreateCell.colSpan = config.metadata.attributes.length; // for IE // todo: do this only for IE
+
+	var createCell = dh.createEl(dh.TD_TAG);
+	var createLink = dh.createEl(dh.A_TAG, null, null, 'Создать');
+	createLink.setAttribute(dh.HREF_ATTRIBUTE, '/create?entityId=' + metadata.id);
+	createCell.appendChild(createLink);
+
+	var rightCreateCell = dh.createEl(dh.TD_TAG, null, null, NBSP_CODE);
+
+	createRow.appendChild(leftCreateCell);
+	createRow.appendChild(createCell);
+	createRow.appendChild(rightCreateCell);
+
+	tableBody.appendChild(createRow);
 
 	table.appendChild(tableBody);
 
