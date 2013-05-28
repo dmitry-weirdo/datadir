e059f2fa650938fbf3d81480c6cb7852df79a4ec
dmitry.weirdo@gmail.com <dmitry.weirdo@gmail.com@8e0f0ab9-060e-e749-fcf0-e6a1db176365>
2013-05-28 19:04:18 +0000

    createTables.sql and dropTables.sql added.

---
+++ trunk/datadirSrv/src/main/sql/dropTables.sql
@@ -0,0 +1,10 @@
+drop table Attribute;
+drop sequence gen_attribute_id;
+
+drop table Entity;
+drop sequence gen_entity_id;
+
+drop table Attribute_type;
+drop sequence gen_attribute_type_id;
+
+commit;
