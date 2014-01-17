5736cc699c1419b47550b8452963fc469095ffdb
dmitry.weirdo@gmail.com <dmitry.weirdo@gmail.com@8e0f0ab9-060e-e749-fcf0-e6a1db176365>
2014-01-17 16:50:46 +0000

    Test SVN tags filling.

---
+++ trunk/datadirSrv/src/main/java/ru/datadir/model/validation/SimpleValidator.java
@@ -0,0 +1,12 @@
+package ru.datadir.model.validation;
+
+/**
+ * Copyright 2014 {@link "mailto:dmitry.weirdo@gmail.com" Dmitriy Popov}.
+ * $HeadURL$
+ * $Author$
+ * $Revision$
+ * $Date::                      $
+ */
+public interface SimpleValidator
+{
+}
